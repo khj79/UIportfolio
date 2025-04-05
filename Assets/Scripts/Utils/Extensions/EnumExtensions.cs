5e9f4c14edb5720c4304b8d424341bf0cc3d533d
kim hyun jun <mycecilia@gmail.com>
2025-04-05 16:56:08 +0900

    ItemRarity ToText를 제네릭으로 변경

---
+++ Assets/Scripts/Utils/Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public static class EnumTextExtensions
+{
+    private static readonly Dictionary<Type, string[]> _enumTextCache = new();
+
+    public static string ToText<TEnum>(this TEnum value) where TEnum : Enum
+    {
+        var type = typeof(TEnum);
+
+        if (!_enumTextCache.TryGetValue(type, out var names))
+        {
+            names = Enum.GetNames(type);
+            _enumTextCache[type] = names;
+        }
+
+        return names[Convert.ToInt32(value)];
+    }
+}
